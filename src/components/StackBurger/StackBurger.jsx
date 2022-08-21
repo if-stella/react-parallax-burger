51a5631c2c8fe24b5f09942ec7f47740161b39fd
Stella Raab <hallo@stellaraab.de>
2022-08-21 15:28:26 +0200

    Removed asset folder, all cloudinary now

--- src/components/StackBurger/StackBurger.jsx
+++ src/components/StackBurger/StackBurger.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './StackBurger.scss'
-import topBun from '../../assets/b01-topbun.png'
-import onions from '../../assets/b02-onions.png'
-import tomatoes from '../../assets/b03-tomatoes.png'
-import patty from '../../assets/b04-patty.png'
-import bottomBun from '../../assets/b05-bottombun.png'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
-
+const images = [
+'https://res.cloudinary.com/dr24t0rw2/image/upload/v1660813857/Burger/b01-topbun_a3ky6x.png',
+'https://res.cloudinary.com/dr24t0rw2/image/upload/v1660813857/Burger/b02-onions_dyxqmr.png',
+'https://res.cloudinary.com/dr24t0rw2/image/upload/v1660813857/Burger/b03-tomatoes_wjrblp.png',
+'https://res.cloudinary.com/dr24t0rw2/image/upload/v1660813857/Burger/b04-patty_fw0qzu.png',
+'https://res.cloudinary.com/dr24t0rw2/image/upload/v1661087423/Burger/berger_9_h3cbrq.png',
+ ];
 
 const StackBurger = () => {
   return (
@@ -15,11 +16,11 @@ const StackBurger = () => {
       <div className="parallax-container">
       <Parallax pages={2}>
         <ParallaxLayer sticky={true}><h2>Only the best!</h2><p>We strive to elevate the burger experience to its top shelf – because you and your burger deserve better. We like them medium rare with a nice pink center and juices running down your arm, but you have it as you like it. We like them with a few premium toppings that are there for a reason.</p></ParallaxLayer>
-        <ParallaxLayer offset={1.35} speed={1} id={'topbun'}><img src={topBun} alt="Burger bun top"/></ParallaxLayer>
-        <ParallaxLayer offset={1.35} speed={2} id={'onions'}><img src={onions} alt="onions"/></ParallaxLayer>
-        <ParallaxLayer offset={1.35} speed={3} id={'tomatoes'}><img src={tomatoes} alt="tomatoes" id="tomatoes"/></ParallaxLayer>
-        <ParallaxLayer offset={1.35} speed={4} id={'patty'}><img src={patty} alt="patty" id="patty"/></ParallaxLayer>
-        <ParallaxLayer offset={1.35} speed={5} id={'bottombun'}><img src={bottomBun} alt="Burger bun bottom" id="bottomBun"/></ParallaxLayer>
+        <ParallaxLayer offset={1.35} speed={1} id={'topbun'}><img src={images[0]} alt="Burger bun top"/></ParallaxLayer>
+        <ParallaxLayer offset={1.35} speed={2} id={'onions'}><img src={images[1]} alt="onions"/></ParallaxLayer>
+        <ParallaxLayer offset={1.35} speed={3} id={'tomatoes'}><img src={images[2]} alt="tomatoes" id="tomatoes"/></ParallaxLayer>
+        <ParallaxLayer offset={1.35} speed={4} id={'patty'}><img src={images[3]} alt="patty" id="patty"/></ParallaxLayer>
+        <ParallaxLayer offset={1.35} speed={5} id={'bottombun'}><img src={images[4]} alt="Burger bun bottom" id="bottomBun"/></ParallaxLayer>
       </Parallax>
       </div>
     </div>
